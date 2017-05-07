2d489808110262542fbb50098204fccbfbb0980b
serenamidori <serenamidori@gmail.com>
2017-05-07 20:01:38 +0000

    Fixed database scripts for local and heroku builds

--- herokuDB.sh
+++ herokuDB.sh
@@ -1,6 +1,7 @@
 echo "Restarting the DB for Heroku..."
 echo "ᕕ( ᐛ )ᕗ Dropping tables and starting over! Seeding with seeds.rb!"
-heroku run rake db:drop:all
-heroku run rake db:create:all
+echo "Important! Reseting requires your input:"
+heroku pg:reset DATABASE
 heroku run rake db:migrate
 heroku run rake db:seed
+echo "All done!"
--- restartDB.sh
+++ restartDB.sh
@@ -1,6 +1,12 @@
 echo "Restarting the DB for Cloud9..."
 echo "ᕕ( ᐛ )ᕗ Dropping tables and starting over! Seeding with seeds.rb!"
-rake db:drop:all
-rake db:create:all
+echo "Restarting postgresql"
+sudo service postgresql stop
+sudo service postgresql start 
+dropdb cards
+echo "Dropped DB 'cards'"
+createdb cards
+echo "Created DB 'cards'"
 rake db:migrate
 rake db:seed
+echo "All done!"
