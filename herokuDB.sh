e0d5f5b0efc6128c65b8a5bc76881e56f0a503fa
serenamidori <serenamidori@gmail.com>
2017-06-25 04:22:44 +0000

    fixed the Heruko Database script

--- herokuDB.sh
+++ herokuDB.sh
@@ -1,7 +1,6 @@
 echo "Restarting the DB for Heroku..."
 echo "ᕕ( ᐛ )ᕗ Dropping tables and starting over! Seeding with seeds.rb!"
-echo "Important! Reseting requires your input:"
-heroku pg:reset DATABASE
+heroku pg:reset DATABASE --confirm werewolf-helper
 heroku run rake db:migrate
 heroku run rake db:seed
 echo "All done!"
